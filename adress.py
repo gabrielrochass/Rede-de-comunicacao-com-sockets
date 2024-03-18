f83cb1c33346cc94c4eb404694e8a318f69fba43
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-03-18 00:02:53 -0300

    fix: organização

---
+++ adress.py
@@ -0,0 +1,4 @@
+# endereços e portas de comunicação entre cliente e servidor.
+# O endereço IP é um número que identifica um dispositivo na rede.
+# A porta é um número que identifica um serviço que está rodando em um dispositivo.
+
