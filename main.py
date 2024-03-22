43fefabd11c1b6a674033f3e886d421217ceb727
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-03-22 03:23:53 -0300

    feat: envia envriptado

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# importa as classes
+# cria os pcs
+# usa a politica de roteamento
+# cria a ca
 import threading
 from pc import PC
 from ca import AutoridadeCertificadora
-import rsa
-  
 ac = AutoridadeCertificadora()
 
 pc1 = PC(nome='PC1', ip='127.0.0.1', porta=1000, id=1)
@@ -29,45 +31,29 @@ pc4.set_key(ac.registrar(pc4.id))
 pc5.set_key(ac.registrar(pc5.id))
 pc6.set_key(ac.registrar(pc6.id))
 
-# printar as chaves privadas e publicas
-# print(f'Chave privada PC1: {pc1.key}')
-# print(f'Chave publica PC1: {ac.consultar_chave_publica(pc1.id)}')
-
-def roteamento(pc_origem, pc_destino, msg, ac):
+def roteamento(pc_origem, pc_destino, msg):
     if pc_destino in vizinhos[pc_origem.nome]: 
-        pc_origem.send(msg, pc_destino.ip, pc_destino.porta)
+        pc_origem.send_encrypted(msg, pc_destino.ip, pc_destino.porta, pc_destino.id, ac)
         return
-    else: # OLHAR AQUI
+    else:
         # printar caminho
-        pc_origem.send_to_ca(msg, pc_destino.ip, pc_destino.porta, pc_destino.id, ac)
-        # oposto = (pc_origem.id + 3) % 6
-        # if oposto == 0:
-        #     oposto = 6
-        # if pc_origem.id > oposto:
-        #     print(f'{pc_origem.nome} -> {vizinhos[pc_origem.nome][1].nome} -> {pc_destino.nome}')
-        #     roteamento(pc_origem, vizinhos[pc_origem.nome][1] , msg) 
-        # else:
-        #     print(f'{pc_origem.nome} -> {vizinhos[pc_origem.nome][0].nome} -> {pc_destino.nome}')
-        #     roteamento(pc_origem, vizinhos[pc_origem.nome][0] , msg)
+        oposto = (pc_origem.id + 3) % 6
+        if oposto == 0:
+            oposto = 6
+        if pc_origem.id > oposto:
+            print(f'{pc_origem.nome} -> {vizinhos[pc_origem.nome][1].nome} -> {pc_destino.nome}')
+            roteamento(pc_origem, vizinhos[pc_origem.nome][1] , msg) 
+        else:
+            print(f'{pc_origem.nome} -> {vizinhos[pc_origem.nome][0].nome} -> {pc_destino.nome}')
+            roteamento(pc_origem, vizinhos[pc_origem.nome][0] , msg)
         
 
 pcs = [pc1, pc2, pc3, pc4, pc5, pc6]
 for pc in pcs:
-    threading.Thread(target=pc.listen).start()
+    threading.Thread(target=pc.listen_encrypted).start()
 
-# Envie mensagens de teste entre PCs
-# pc1.send(msg='Hello from PC1', ip='127.0.0.3', porta=3000)  # Envie uma mensagem de PC1 para PC2
-# pc3.send(msg='Hello from PC3', ip='127.0.0.1', porta=1000)     
 
-# Envie mensagens de teste entre PCs com roteamento
-roteamento(pc1, pc2, 'Hello from PC1', ac)  # Envie uma mensagem de PC1 para PC3
+pc1.send_encrypted('Hello from PC1', pc2.ip, pc2.porta, pc2.id, ac)
+# roteamento(pc1, pc2, 'Hello from PC1') 
 # roteamento(pc3, 'PC1', 'Hello from PC3')  # Envie uma mensagem de PC3 para PC1
 
-# envia mensagem criptografada
-# envia mensagem criptografada
-# mensagem = 'Hello from PC1'
-# mensagem_criptografada = rsa.encrypt(mensagem.encode(), ac.consultar_chave_publica(pc2.id))
-# pc1.send(msg=mensagem_criptografada, ip='127.0.0.2', porta=2000)
-ca_thread = threading.Thread(target=ac.listen).start()
-
-# roteamento(pc1, pc3, 'Hello from PC1', ac)
--- pc.py
+++ pc.py
@@ -1,8 +1,8 @@
 # classe de pc pra impotar main
+# classe de pc pra impotar main
 import socket
 import threading
 import rsa
-from ca import AutoridadeCertificadora
 
 class PC:
     def __init__(self, nome, ip, porta, id):
@@ -11,7 +11,19 @@ class PC:
         self.porta = porta
         self.id = id
 
-    def listen(self):
+    def set_key(self, key):
+        self.key = key
+        return self.key
+    
+    def send_encrypted(self, msg, ip, porta, id, ca):
+        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        chave_publica_dest = ca.consultar_chave_publica(id)
+        text = rsa.encrypt(msg.encode(), chave_publica_dest)
+        con.connect((ip, porta))
+        con.send(text)
+        con.close()
+
+    def listen_encrypted(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.ip, self.porta))
         self.socket.listen()
@@ -19,31 +31,9 @@ class PC:
 
         while True:
             con, _ = self.socket.accept()
-            msg = con.recv(2048).decode()
-            ciphertext = rsa.decrypt(msg,self.key)
-            print(f'{self.nome} recebeu: {ciphertext.hex()}')
+            msg = con.recv(2048)
+            decrypted_msg = rsa.decrypt(msg, self.key)
+            print(f'{self.nome} recebeu: {decrypted_msg.decode()}')
             con.close()
+
         
-    def send(self, msg, ip, porta):
-        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        con.connect((ip, porta))
-        con.send(msg.encode())
-        con.close()
-
-    def set_key(self, key):
-        self.key = key
-        return self.key
-    
-    def send_to_ca(self, msg, ca_ip, ca_porta, destinatario_id, ca):
-        chave_publica_destinatario = ca.consultar_chave_publica(destinatario_id)
-        if chave_publica_destinatario is None:
-            print(f"Erro: Chave pública do destinatário não encontrada.")
-            return
-
-        msg_criptografada = rsa.encrypt(msg.encode(), chave_publica_destinatario)
-
-        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        con.connect((ca_ip, ca_porta))
-        con.send(msg_criptografada)
-        con.close()
-
